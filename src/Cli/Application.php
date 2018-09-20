97c2956cd75e787bbea2fb1768d3bb3c6398afcd
Kévin Gomez <contact@kevingomez.fr>
2018-09-20 16:41:53 +0200

    Give better default values for the CLI arguments

--- src/Cli/Application.php
+++ src/Cli/Application.php
@@ -13,6 +13,8 @@ use Pimple\Psr11\Container as Psr11Container;
 
 class Application extends Silly
 {
+    private const DEFAULT_BOUCLE = 'boucle.yaml';
+
     public function __construct(\Pimple\Container $container)
     {
         parent::__construct('Boucle');
@@ -38,24 +40,23 @@ class Application extends Silly
             ->descriptions('Dumps the default configuration for a boucle.');
 
         $this
-            ->command('steps:list boucle', function (string $boucle, OutputInterface $output): void {
+            ->command('steps:list [boucle]', function (string $boucle = self::DEFAULT_BOUCLE, OutputInterface $output): void {
                 $this->service(Command\ListSteps::class)->run($output, $boucle);
             })
             ->descriptions('List the steps in a given boucle.');
 
         $this
-            ->command('build boucle [webroot]', function (string $boucle, string $webRoot, OutputInterface $output): void {
-                if (!is_dir($webRoot)) {
-                    throw new \RuntimeException("Directory '$webRoot' does not exist.");
+            ->command('build [boucle] [webroot]', function (string $boucle = self::DEFAULT_BOUCLE, string $webroot = '.', OutputInterface $output): void {
+                if (!is_dir($webroot)) {
+                    throw new \RuntimeException("Directory '$webroot' does not exist.");
                 }
 
-                $this->service(Command\Build::class)->run($output, $boucle, $webRoot);
-                $this->service(Command\CopyDist::class)->run($output, $webRoot);
+                $this->service(Command\Build::class)->run($output, $boucle, $webroot);
+                $this->service(Command\CopyDist::class)->run($output, $webroot);
 
                 $output->writeln('Done.');
             })
-            ->descriptions('Builds the map and galleries.')
-            ->defaults(['webroot' => '.']);
+            ->descriptions('Builds the map and galleries.');
     }
 
     private function service(string $service)
